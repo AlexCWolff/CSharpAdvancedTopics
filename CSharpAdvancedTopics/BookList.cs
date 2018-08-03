0a1bdee5039b261c5065c3e8f855332525496d8c
Alex C. Wolff <alexcwolff@protonmail.ch>
2018-08-03 12:55:37 -0400

    Generics

---
+++ CSharpAdvancedTopics/BookList.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace CSharpAdvancedTopics
+{
+    // Object specific implementation, requires code duplication
+    public class BookList 
+    {
+        public void Add(Book book)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Book this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    // Casting implementation, no duplication but inefficient 
+    public class ObjectList 
+    {
+        public void Add(object value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    // Generic implementation
+    public class GenericList<T>
+    {
+        public void Add(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T this[int index]
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+
+    // Generic dictionary
+    public class GenericDictionary<TKey, TValue>
+    {
+        public void Add(TKey key, TValue value)
+        {
+
+        }
+    }
+}
